f157d0835a9d64aec3a1c58d68b8fd884513e7ea
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2026-08-29 00:10:43 -0300

    Adicionado dockerfile

---
+++ README.md
@@ -0,0 +1,5 @@
+## Como executar
+
+1. Crie o .env
+2. Rode `docker build -t cp .`
+3. Rode `docker run --env-file .env cp`
--- src/main.py
+++ src/main.py
@@ -1,64 +1,60 @@
-########################################################
-######################## AGENTE ########################
-########################################################
+from crewai import Agent, Task, Process, Crew
+import paho.mqtt.client as mqtt
 
-# from crewai import Agent, Task, Process, Crew
-
-
-# def criar_agente():
-    # relator = Agent(
-    #     role="Analista de dados de computação de borda",
-    #     goal="Analisar dados e criar relatórios com base nas especificações do sistema",
-    #     backstory=(
-    #         "Você é um analista de dados sênior especializado em fazer relatórios "
-    #         "corporativos para insights úteis."
-    #     ),
-    #     verbose=True
-    # )
+def criar_agente():
+    relator = Agent(
+        role="Analista de dados de computação de borda",
+        goal="Analisar dados e criar relatórios com base nas especificações do sistema",
+        backstory=(
+            "Você é um analista de dados sênior especializado em fazer relatórios "
+            "corporativos para insights úteis."
+        ),
+        verbose=True
+    )
     
-    # return relator
+    return relator
 
-# def criar_task(agente, specs, dados):
-    # analisar_dados = Task(
-    #     description=(
-    #     f"""
-    #         Considere as especificações abaixo:
-    #         {specs}
+def criar_task(agente, specs, dados):
+    analisar_dados = Task(
+        description=(
+        f"""
+            Considere as especificações abaixo:
+            {specs}
             
-    #         Analise os dados advindos abaixo:
-    #         {dados}
+            Analise os dados advindos abaixo:
+            {dados}
             
-    #         Identifique:
-    #         1. Se os dados estão dentro da especificação;
-    #         2. Impacto no negócio;
-    #         3. Possíveis causas;
-    #         4. Informações adicionais necessárias;
-    #         5. Próxima ação recomendada.
+            Identifique:
+            1. Se os dados estão dentro da especificação;
+            2. Impacto no negócio;
+            3. Possíveis causas;
+            4. Informações adicionais necessárias;
+            5. Próxima ação recomendada.
             
-    #         Caso o sensor apresente dados fora da especificação, direcione o relatório para a equipe de sustentação. Caso contrário, direcione-o à diretoria.
-    #     """
-    #     ),
-    #     expected_output=(
-    #         "Um relatório em Markdown com destinatário, diferença das especificações, impacto, "
-    #         "possíveis causas e próximos passos. Caso os dados estejam dentro das especificações, "
-    #         "informe os impactos positivos no lugar da diferença das especificações."
-    #     ),
-    #     agent=agente
-    # )
+            Caso o sensor apresente dados fora da especificação, direcione o relatório para a equipe de sustentação. Caso contrário, direcione-o à diretoria.
+        """
+        ),
+        expected_output=(
+            "Um relatório em Markdown com destinatário, diferença das especificações, impacto, "
+            "possíveis causas e próximos passos. Caso os dados estejam dentro das especificações, "
+            "informe os impactos positivos no lugar da diferença das especificações."
+        ),
+        agent=agente
+    )
     
-    # return analisar_dados
+    return analisar_dados
 
-# def criar_equipe(agentes, tasks):
-    # equipe = Crew(
-    #     agents=agentes,
-    #     tasks=tasks,
-    #     process=Process.sequential,
-    #     verbose=True
-    # )
+def criar_equipe(agentes, tasks):
+    equipe = Crew(
+        agents=agentes,
+        tasks=tasks,
+        process=Process.sequential,
+        verbose=True
+    )
     
-    # return equipe
+    return equipe
 
-# relator = criar_agente()
+relator = criar_agente()
 
 specs = """
     Os dados do sensor medem a quantidade de verde em uma área, de 0 a 100%.
@@ -72,12 +68,6 @@ specs = """
     
     Dados aceitáveis são os dados acima de 60%. Ou seja, valores considerados "Bom" e "Excelente".
 """
-
-########################################################
-######################### MQTT #########################
-########################################################
-
-import paho.mqtt.client as mqtt
 
 BROKER_URL = "test.mosquitto.org"
 BROKER_PORT = 1883
@@ -95,20 +85,20 @@ def on_message(client, userdata, message):
     
     print(f"Mensagem recebida: {str(message.payload.decode('utf-8'))} no tópico {message.topic}")
     
-    # analisar_dados = criar_task(relator, specs, dados)
-    # equipe = criar_equipe([relator], [analisar_dados])
+    analisar_dados = criar_task(relator, specs, dados)
+    equipe = criar_equipe([relator], [analisar_dados])
     
-    # resultado = equipe.kickoff(
-    #     inputs={
-    #         "chamado":(
-    #             "O sensor retornou uma nova leitura de dados hoje. "
-    #             "Analise dentro das especificações os dados recém-chegados "
-    #             "e como está afetando a empresa."
-    #         )
-    #     }
-    # )
+    resultado = equipe.kickoff(
+        inputs={
+            "chamado":(
+                "O sensor retornou uma nova leitura de dados hoje. "
+                "Analise dentro das especificações os dados recém-chegados "
+                "e como está afetando a empresa."
+            )
+        }
+    )
 
-    # print(resultado.raw)
+    print(resultado.raw)
 
 client.on_message = on_message
 
